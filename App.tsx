1e3be7c7c601617fe30dc0313716de85d097df3a
janghyeokCho-hub <janghyeok9257@gmail.com>
2023-03-13 17:33:44 +0900

    Tab icon change

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { TabParamList } from "./src/types";
 import HomeScreen from "./src/screens/Home";
 
@@ -7,7 +8,27 @@ const Tab = createBottomTabNavigator<TabParamList>();
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = "home";
+            } else if (route.name === "Settings") {
+              iconName = "settings";
+            }
+
+            return (
+              <MaterialCommunityIcons
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          },
+        })}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
       </Tab.Navigator>
     </NavigationContainer>
